e610852f62bad1ce2d984a01babaf86360a08d5a
Baijnath Kumar <bk0491731@gmail.com>
2026-05-11 02:48:32 +0530

    layout changes update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
+   <html
+      lang="en"
+      className="h-full antialiased"
+      suppressHydrationWarning
+    >
       <body className="min-h-full bg-slate-50 text-slate-950">
         <Providers>
           <Navbar />
